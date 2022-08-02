2404f7dd7ba6c645fe1d8ebba5a72f6a2f50a7b1
ONEASH <oneash0824@naver.com>
2022-08-02 22:31:30 +0900

    Question Detail Template

--- src/main/java/com/mysite/SBBPractice/service/QuestionService.java
+++ src/main/java/com/mysite/SBBPractice/service/QuestionService.java
@@ -1,11 +1,14 @@
 package com.mysite.SBBPractice.service;
 
+import com.mysite.SBBPractice.DataNotFoundException;
 import com.mysite.SBBPractice.domain.Question;
 import com.mysite.SBBPractice.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +18,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
